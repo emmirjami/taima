22c157a0811927da1cfb1730c88a0364897322c5
emmirjami <104158402+emmirjami@users.noreply.github.com>
2022-12-30 22:21:01 +0200

    Implement button logic

--- assets/main.js
+++ assets/main.js
@@ -5,6 +5,24 @@ var thoughts = [
     "haistan hiiren"
 ];
 
+var scratchThoughts = [
+    "hih, kutittaa",
+    "lisää, lisää!",
+    "hyviä rapsuja"
+];
+
+var treatThoughts = [
+    "nam, herkkua",
+    "ottaisin toisen",
+    "rouskis"
+];
+
+var playThoughts = [
+    "squeeeek t. paprika",
+    "heitä lelu, mutta älä ota sitä minulta",
+    "grrr, grrr"
+];
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -16,9 +34,30 @@ function newWoof() {
     title.innerHTML = woofs[getRandomInt(0, woofs.length - 1)];
 }
 
-function newThought() { 
-    var thought = document.getElementById("thought");
-    thought.innerHTML = "💭 " + thoughts[getRandomInt(0, thoughts.length - 1)];
+function newThought(clickThought) {
+    var thought = clickThought;
+
+    if (!thought) {
+        thought = thoughts[getRandomInt(0, thoughts.length - 1)];
+    }
+
+    var thoughtBox = document.getElementById("thought");
+    thoughtBox.innerHTML = "💭 " + thought;
+}
+
+function scratchButtonClicked() {
+    var thought = scratchThoughts[getRandomInt(0, scratchThoughts.length - 1)];
+    newThought(thought);
+}
+
+function treatButtonClicked() {
+    var thought = treatThoughts[getRandomInt(0, treatThoughts.length - 1)];
+    newThought(thought);
+}
+
+function playButtonClicked() {
+    var thought = playThoughts[getRandomInt(0, playThoughts.length - 1)];
+    newThought(thought);
 }
 
 newWoof();
